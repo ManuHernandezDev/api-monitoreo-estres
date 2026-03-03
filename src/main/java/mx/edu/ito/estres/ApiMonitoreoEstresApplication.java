7f144510e99cd4de7cdd1fa830361bddab73ffae
Manuel Hernandez Soriano <manu.hdz.dev@gmail.com>
2026-03-02 19:13:24 -0600

    chore: inicializar proyecto Spring Boot base para monitoreo de estres

---
+++ src/main/java/mx/edu/ito/estres/ApiMonitoreoEstresApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.ito.estres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonitoreoEstresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMonitoreoEstresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/ito/estres/ApiMonitoreoEstresApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.ito.estres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonitoreoEstresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
