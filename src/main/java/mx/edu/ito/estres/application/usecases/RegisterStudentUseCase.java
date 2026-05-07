f1745e5be36da26841d90040d9ddcbe0910bdec0
Manuel Hernandez <manu.hdz.dev@gmail.com>
2026-05-06 23:54:00 -0600

    feat: encrypt student passwords with bcrypt

--- src/main/java/mx/edu/ito/estres/application/usecases/RegisterStudentUseCase.java
+++ src/main/java/mx/edu/ito/estres/application/usecases/RegisterStudentUseCase.java
@@ -6,12 +6,15 @@ import mx.edu.ito.estres.domain.exception.EmailAlreadyExistsException;
 import mx.edu.ito.estres.application.ports.out.StudentRepositoryPort;
 import mx.edu.ito.estres.domain.exception.InvalidStudentException;
 import mx.edu.ito.estres.domain.model.Student;
+import mx.edu.ito.estres.infrastructure.config.PasswordEncoderConfig;
 
 public class RegisterStudentUseCase {
     private final StudentRepositoryPort studentRepositoryPort;
+    private final PasswordEncoderConfig passwordEncoderConfig;
 
-    public RegisterStudentUseCase(StudentRepositoryPort studentRepositoryPort) {
+    public RegisterStudentUseCase(StudentRepositoryPort studentRepositoryPort, PasswordEncoderConfig passwordEncoderConfig) {
         this.studentRepositoryPort = studentRepositoryPort;
+        this.passwordEncoderConfig = passwordEncoderConfig;
     }
 
     public Student register(Student student) {
@@ -22,8 +25,7 @@ public class RegisterStudentUseCase {
         if (existingStudent.isPresent()) {
             throw new EmailAlreadyExistsException();
         }
-
+        passwordEncoderConfig.passwordEncoder().encode(student.password());
         return studentRepositoryPort.save(student);
-
     }
 }
