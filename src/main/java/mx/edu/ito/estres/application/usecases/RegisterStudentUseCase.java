030470be0e3059d924fd7d705804ccd8c58c642b
Manuel Hernandez <manu.hdz.dev@gmail.com>
2026-05-07 00:43:04 -0600

    feat: configuration JWT with passwored encoder

--- src/main/java/mx/edu/ito/estres/application/usecases/RegisterStudentUseCase.java
+++ src/main/java/mx/edu/ito/estres/application/usecases/RegisterStudentUseCase.java
@@ -6,6 +6,7 @@ import mx.edu.ito.estres.domain.exception.EmailAlreadyExistsException;
 import mx.edu.ito.estres.application.ports.out.StudentRepositoryPort;
 import mx.edu.ito.estres.domain.exception.InvalidStudentException;
 import mx.edu.ito.estres.domain.model.Student;
+import mx.edu.ito.estres.infrastructure.adapters.in.web.dto.StudentRequestDTO;
 import mx.edu.ito.estres.infrastructure.config.PasswordEncoderConfig;
 
 public class RegisterStudentUseCase {
@@ -17,15 +18,28 @@ public class RegisterStudentUseCase {
         this.passwordEncoderConfig = passwordEncoderConfig;
     }
 
-    public Student register(Student student) {
-        if (student == null) {
-            throw new InvalidStudentException("Student cannot be null");
-        }
-        Optional<Student> existingStudent = studentRepositoryPort.findByEmail(student.email());
-        if (existingStudent.isPresent()) {
+    public Student register(StudentRequestDTO studentRequestDTO) {
+        // 1. Validaciones (Ya las hace tu constructor de Record, ¡bien!)
+
+        // 2. Verificar duplicados
+        if (studentRepositoryPort.findByEmail(studentRequestDTO.email()).isPresent()) {
             throw new EmailAlreadyExistsException();
         }
-        passwordEncoderConfig.passwordEncoder().encode(student.password());
-        return studentRepositoryPort.save(student);
+
+        // 3. Encriptar password y crear NUEVO objeto (porque el record es inmutable)
+        String encodedPassword = passwordEncoderConfig.passwordEncoder().encode(studentRequestDTO.password());
+
+        Student studentToSave = new Student(
+                null, // El ID va null porque lo genera la DB
+                studentRequestDTO.email(),
+                encodedPassword, // Guardamos la encriptada
+                studentRequestDTO.semester(),
+                studentRequestDTO.origin(),
+                studentRequestDTO.sleepHours(),
+                studentRequestDTO.privacyAccepted(),
+                studentRequestDTO.role() // <--- ASEGÚRATE DE PASAR EL ROL AQUÍ
+        );
+
+        return studentRepositoryPort.save(studentToSave);
     }
 }
