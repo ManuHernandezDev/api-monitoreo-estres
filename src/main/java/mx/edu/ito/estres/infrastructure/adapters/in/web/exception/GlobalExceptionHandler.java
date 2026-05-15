906a7ea65568cb31d100d96c1e1207afdeb4deed
Manuel Hernandez <manu.hdz.dev@gmail.com>
2026-05-14 19:42:49 -0600

    feat(docs): configure swagger openapi ui

--- src/main/java/mx/edu/ito/estres/infrastructure/adapters/in/web/exception/GlobalExceptionHandler.java
+++ src/main/java/mx/edu/ito/estres/infrastructure/adapters/in/web/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package mx.edu.ito.estres.infrastructure.adapters.in.web.exception;
 
 import mx.edu.ito.estres.domain.exception.EmailAlreadyExistsException;
+import mx.edu.ito.estres.domain.exception.InvalidCredentialsException;
+import mx.edu.ito.estres.domain.exception.StudentNotFoundException;
+import mx.edu.ito.estres.infrastructure.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -44,5 +47,57 @@ public class GlobalExceptionHandler {
                 );
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(
+            InvalidCredentialsException.class
+    )
+    public ResponseEntity<ErrorResponse>
+    handleInvalidCredentials(
+            InvalidCredentialsException ex
+    ) {
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        new ErrorResponse(
+                                ex.getMessage(),
+                                401
+                        )
+                );
+    }
+
+    @ExceptionHandler(
+            StudentNotFoundException.class
+    )
+    public ResponseEntity<ErrorResponse>
+    handleStudentNotFound(
+            StudentNotFoundException ex
+    ) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(
+                        new ErrorResponse(
+                                ex.getMessage(),
+                                404
+                        )
+                );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse>
+    handleGeneric(
+            Exception ex
+    ) {
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        new ErrorResponse(
+                                ex.getMessage(),
+                                500
+                        )
+                );
     }
 }
